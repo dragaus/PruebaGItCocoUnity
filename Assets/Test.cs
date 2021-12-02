cd460f22bff4c44c5272b82694739cbef155a28a
dragaus <dragalichi@gmail.com>
2021-12-02 09:31:43 -0600

    Fix issues

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,7 +9,6 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Coco");
         Debug.Log("Hello World");
     }
 
